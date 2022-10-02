c3232176bc6842ea3214a4d23748ec26d08b7684
itworlddev2021 <itworlddev2021@gmail.com>
2022-10-02 16:37:36 +0630

    change url

--- src/app/service/funct.service.ts
+++ src/app/service/funct.service.ts
@@ -8,8 +8,8 @@ export class FunctService {
   public ipaddress: any;
 
   constructor() {  
+    //this.ipaddress = "http://localhost:3000/"; 
     
-    this.ipaddress = "http://localhost:3000/"; 
-
+     this.ipaddress = "https://pos-123.herokuapp.com:3000/";
   }
 }
